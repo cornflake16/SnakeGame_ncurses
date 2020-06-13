1820fa8f261c0b56a053d22344ff478f435b45f4
Nakwon Yun <skrdnjs1102@gmail.com>
2020-06-13 17:38:25 +0900

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,17 +1,19 @@
 #include "Stage.h"
 int **map;
+int key;
+pair<int, int> gate[2];
 int main()
 {
+    srand((unsigned)time(0));
     Stage view = Stage();
     view.setMap(view.stage);
     map = view.copyMap(0);
-    while (1)
+    view.appearGate(map);
+    view.appearItem(map);
+    do
     {
-        int key = getch();
-        view.drawMap(view.getMapRow(), view.getMapCol(), map);
-        if (key == 27)
-            break;
-    }
+        view.drawMap(map);
+    } while ((key = getch()) != 27);
     endwin();
     return 0;
 }
