c15f204b44ced351872b45ebeea96729be207dec
Nakwon Yun <skrdnjs1102@gmail.com>
2020-06-16 00:22:13 +0900

    Update Stage.cpp
     <수정>
     - 호환성 개선 (Stage 클래스의 private 상수 값과 define된 항목의 인덱스들만 바꿔도 올바르게 동작 가능)
     - 키 연속 입력시 화면이 깜빡이는 현상 해결
    <추가>
     - 스테이지 중간 벽 설정 및 스테이지 변경
     - 스코어/미션 보드 관련 로직 구현 및 출력
     - 게임종료와 스테이지 클리어시 alert 출력

--- Stage.cpp
+++ Stage.cpp
@@ -1,8 +1,7 @@
 #include "Stage.h"
-WINDOW *create_newwin(int height, int width, int starty, int startx);
 Stage::Stage()
 {
-    finish = false;
+    srand((unsigned)time(0));
     initscr();
     resize_term(NLINES, NCOLS);
     keypad(stdscr, TRUE);
@@ -31,14 +30,13 @@ Stage::Stage()
     init_pair(SNAKE_BODY, COLOR_CYAN, COLOR_BLUE);       //4(뱀 몸): 형광, 파랑
     init_pair(GROWTH_ITEM, COLOR_YELLOW, COLOR_RED);     //5(+ 아이템): 노랑, 빨강
     init_pair(POISON_ITEM, COLOR_RED, COLOR_MAGENTA);    //6(- 아이템) 빨강, 분홍
-    init_pair(GATE, COLOR_CYAN, COLOR_CYAN);             //7(목표+캐릭터):  형광, 형광
+    init_pair(GATE, COLOR_CYAN, COLOR_CYAN);             //8(목표+캐릭터):  형광, 형광
 }
 Stage::~Stage()
 {
     delwin(mission);
     delwin(score);
     delwin(game);
-    delwin(win);
     int i, j;
     for (i = 0; i < STAGE_NUM; i++)
     {
@@ -47,9 +45,6 @@ Stage::~Stage()
         delete[] stage[i];
     }
     delete[] stage;
-}
-void Stage::drawMission()
-{
 }
 
 void Stage::setMap(int ***&stage)
@@ -71,21 +66,32 @@ void Stage::setMap(int ***&stage)
         {
             for (k = 0; k < MAP_COL; k++)
             {
-                if (!j || !k || j == MAP_ROW - 1 || k == MAP_COL - 1)
+                if (!j || !k || j == ROW_END || k == COL_END)
                     stage[i][j][k] = WALL;
                 else
                     stage[i][j][k] = EMPTY;
             }
         }
         stage[i][0][0] = IMMUNE_WALL;
-        stage[i][0][MAP_COL - 1] = IMMUNE_WALL;
-        stage[i][MAP_ROW - 1][0] = IMMUNE_WALL;
-        stage[i][MAP_ROW - 1][MAP_COL - 1] = IMMUNE_WALL;
+        stage[i][0][COL_END] = IMMUNE_WALL;
+        stage[i][ROW_END][0] = IMMUNE_WALL;
+        stage[i][ROW_END][COL_END] = IMMUNE_WALL;
+        if (i == 0 || i == 2) // 1단계, 3단계에서 중간벽 설정
+        {
+            for (int z = 0; z < 20; z++)
+            {
+                stage[i][MAP_ROW / 2 - 3][MAP_COL / 3 + z] = WALL;
+            }
+        }
     }
 }
 
 int **Stage::copyMap(int nStage)
 {
+    finish = false;
+    memset(stat, 0, sizeof(stat));
+    memset(statMission, 0, sizeof(statMission));
+    memset(chkMission, ' ', sizeof(chkMission));
     int **m = new int *[MAP_ROW];
     for (int i = 0; i < MAP_COL; i++)
         m[i] = new int[MAP_COL];
@@ -101,10 +107,7 @@ int **Stage::copyMap(int nStage)
 
 void Stage::drawMap(int **map)
 {
-    string itemIndex = " ^X@=+-%";
-    int height = NLINES / 1.5 - 1, width = NCOLS / 1.5 - 23;
-
-    game = newwin(height, width, 7, 11);
+    game = newwin(MAP_ROW, MAP_COL, 5, 6);
     for (int i = 0; i < MAP_ROW; i++)
     {
         for (int j = 0; j < MAP_COL; j++)
@@ -116,20 +119,88 @@ void Stage::drawMap(int **map)
         }
         printw("\n");
     }
-    drawBorders();
+
+    score = newwin(16, 30, 1, 64);
+    box(score, 0, 0);
+    mvwprintw(score, 0, 10, "[ SCORE ]");
+    mvwprintw(score, 3, 5, "B: %d / 10", stat[0]);
+    mvwprintw(score, 6, 5, "+: %d", stat[1]);
+    mvwprintw(score, 9, 5, "-: %d", stat[2]);
+    mvwprintw(score, 12, 5, "G: %d", stat[3]);
+
+    mission = newwin(16, 30, 18, 64);
+    box(mission, 0, 0);
+    mvwprintw(mission, 0, 9, "[ MISSION ]");
+    mvwprintw(mission, 3, 5, "B: %d ( %c )", statMission[0], chkMission[0]);
+    mvwprintw(mission, 6, 5, "+: %d ( %c )", statMission[1], chkMission[1]);
+    mvwprintw(mission, 9, 5, "-: %d ( %c )", statMission[2], chkMission[2]);
+    mvwprintw(mission, 12, 5, "G: %d ( %c )", statMission[3], chkMission[3]);
+
+    refresh();
     wrefresh(game);
-    mvprintw(3, 80, "[ SCORE ]");
-    mvprintw(20, 79, "[ MISSION ]");
+    wrefresh(score);
+    wrefresh(mission);
+}
+
+pair<int, int> Stage::numOfItems(int **map)
+{
+    int nGrowth = 0, nPoison = 0;
+    for (int i = 0; i < MAP_ROW; i++)
+    {
+        for (int j = 0; j < MAP_COL; j++)
+        {
+            if (map[i][j] == GROWTH_ITEM)
+                nGrowth++;
+            else if (map[i][j] == POISON_ITEM)
+                nPoison++;
+        }
+    }
+    pair<int, int> pii = make_pair(nGrowth, nPoison);
+    return pii;
+}
+
+void Stage::setMission()
+{
+    statMission[0] = rand() % 5 + 6; // 뱀 길이: 5~10
+    statMission[1] = rand() % 5 + 4; // 증가 아이템 획득 횟수: 4~8
+    statMission[2] = rand() % 4 + 3; // 감소 아이템 획득 횟수: 3~6
+    statMission[3] = rand() % 5 + 1; // 게이트 진출 횟수: 1 ~ 5 
+}
+
+bool Stage::isMissionClear()
+{
+    int count = 0;
+    if (stat[0] >= statMission[0])
+        chkMission[0] = 'v';
+    else
+        chkMission[0] = ' ';
+    if (stat[1] >= statMission[1])
+        chkMission[1] = 'v';
+    if (stat[2] >= statMission[2])
+        chkMission[2] = 'v';
+    if (stat[3] >= statMission[3])
+        chkMission[3] = 'v';
+    for (int i = 0; i < 4; i++)
+    {
+        if (chkMission[i] == 'v')
+            count++;
+    }
+    if (count == 4)
+        return TRUE;
+    return FALSE;
 }
 
 void Stage::appearItem(int **&map)
 {
-    int nItem = rand() % 3 + 1;
-    pair<int, int> item[nItem];
+    pair<int, int> nItems = numOfItems(map);
     int row, col;
-    for (int i = 0; i < sizeof(item) / sizeof(item[0]); i++)
+    for (int i = 0; i < 3 - (nItems.first + nItems.second); i++)
     {
-        int itemType = rand() % 2 + 5;
+        int itemType;
+        if (nItems.first > nItems.second)
+            itemType = POISON_ITEM;
+        else
+            itemType = GROWTH_ITEM;
         while (1)
         {
             row = rand() % MAP_ROW;
@@ -137,54 +208,49 @@ void Stage::appearItem(int **&map)
             if (map[row][col] == EMPTY)
                 break;
         }
-        item[i].first = row;
-        item[i].second = col;
-        map[item[i].first][item[i].second] = itemType;
+        map[row][col] = itemType;
     }
 }
 
 void Stage::appearGate(int **&map)
 {
-    pair<int, int> gate[2];
-    int n, row, col;
-    int y, x;
+    int n, y, x;
     for (int i = 0; i < 2; i++)
     {
         while (1)
         {
-            n = rand() % 4;
-            row = rand() % MAP_ROW;
-            col = rand() % MAP_COL;
+            n = rand() % 5;
+            y = rand() % MAP_ROW;
+            x = rand() % MAP_COL;
             switch (n)
             {
             case 0: // 상단 벽
                 y = 0;
-                x = col;
                 break;
             case 1: // 좌측
-                y = row;
                 x = 0;
                 break;
             case 2: // 우측
-                y = row;
-                x = MAP_COL - 1;
+                x = COL_END;
                 break;
             case 3: // 하단
-                y = MAP_ROW - 1;
-                x = col;
+                y = ROW_END;
                 break;
+            case 4: // 중간
+                y = MAP_ROW / 2 - 3;
+                x = MAP_COL / 3 + (rand() % 20);
             }
             if (map[y][x] == WALL)
                 break;
         }
-        gate[i].first = y;
-        gate[i].second = x;
-        map[gate[i].first][gate[i].second] = GATE;
-        if(i == 0){
-          gate1 = new Something(y,x,GATE);
+        map[y][x] = GATE;
+        if (i == 0)
+        {
+            gate1 = new Something(y, x, GATE);
         }
-        if(i == 1){
-          gate2 = new Something(y,x,GATE);
+        if (i == 1)
+        {
+            gate2 = new Something(y, x, GATE);
         }
     }
 }
@@ -197,201 +263,260 @@ void Stage::resume()
 {
 }
 
-void Stage::Move(int direction,int** map)
+void Stage::move(int dir, int **map)
 {
-  map[Bam->y][Bam->x] = 0;
-  Something* q = Bam;
-  Something* p = Bam->link;
-  while(p->link != NULL){
-    q->x = p->x; q->y = p->y;
-    q = p;
-    p = p->link;
-  }
-  if(direction == 65 || direction == 97){
-    map[p->y][p->x] = q->who;
-    q->x = p->x; q->y = p->y;
-    p->x -=1;
-  }
-  else if(direction == 87 || direction == 119){
-    map[p->y][p->x] = q->who;
-    q->x = p->x; q->y = p->y;
-    p->y -=1;
-  }
-  else if(direction == 68 || direction == 100){
-    map[p->y][p->x] = q->who;
-    q->x = p->x; q->y = p->y;
-    p->x +=1;
-  }
-  else if(direction == 83 || direction == 115){
-    map[p->y][p->x] = q->who;
-    q->x = p->x; q->y = p->y;
-    p->y +=1;
-  }
-  if(map[p->y][p->x] == 5 || map[p->y][p->x] == 6){
-    EatItem(map[p->y][p->x],direction,map);
-  }
-  if(map[p->y][p->x] == 1 || map[p->y][p->x] == 2 || map[p->y][p->x] == 4){
-    Gameover();
-  }
-  if(map[p->y][p->x] == GATE){
-    Gate(p,map,direction);
-  }
-  map[p->y][p->x] = p->who;
-}
-
-void Stage::drawBorders()
-{
-    refresh();
-    create_newwin(NLINES - 1, NCOLS - 1, 0, 0);
-    create_newwin(16, 30, 3, 70);
-    create_newwin(16, 30, 20, 70);
-}
-
-WINDOW *create_newwin(int height, int width, int starty, int startx)
-{
-    WINDOW *local_win;
-
-    local_win = newwin(height, width, starty, startx);
-    box(local_win, 0, 0);
-
-    wrefresh(local_win);
-    return local_win;
-}
-
-void Stage::makeSnake(int **map){
-  Body = 3;
-  string itemIndex = " ^X@=+-%";
-  int row = 13;
-  int col = 26;
-  Bam = new Something(row,col--,SNAKE_BODY);
-  Something* p = new Something(row,col--,SNAKE_BODY);
-  Bam -> link = p;
-  p = new Something(row,col--,SNAKE_HEAD);
-  Bam -> link -> link = p;
-  map[Bam->y][Bam->x] = Bam->who;
-  p = Bam-> link;
-  map[p->y][p->x] = p->who;
-  p = p->link;
-  map[p->y][p->x] = p->who;
-}
-
-void Stage::EatItem(int item,int dir,int** map){
-  if(item == 5){
-    if(Body == 10){
-      return;
+    map[Bam->y][Bam->x] = EMPTY;
+    Something *q = Bam;
+    Something *p = q->link;
+    while (p->link != NULL)
+    {
+        q->x = p->x;
+        q->y = p->y;
+        q = p;
+        p = p->link;
     }
-    Something* p = new Something(Bam->y,Bam->x,4);
-    if(dir == 65)
-      p->x++;
-    else if(dir == 87)
-      p->y++;
-    else if(dir == 68)
-      p->x--;
-    else if(dir == 83)
-      p->y--;
-    p->link = Bam;
-    Bam = p;
+    if (dir == LEFT || dir == 97)
+    {
+        map[p->y][p->x] = q->who;
+        q->x = p->x;
+        q->y = p->y;
+        p->x--;
+    }
+    else if (dir == UP || dir == 119)
+    {
+        map[p->y][p->x] = q->who;
+        q->x = p->x;
+        q->y = p->y;
+        p->y--;
+    }
+    else if (dir == RIGHT || dir == 100)
+    {
+        map[p->y][p->x] = q->who;
+        q->x = p->x;
+        q->y = p->y;
+        p->x++;
+    }
+    else if (dir == DOWN || dir == 115)
+    {
+        map[p->y][p->x] = q->who;
+        q->x = p->x;
+        q->y = p->y;
+        p->y++;
+    }
+    if (map[p->y][p->x] == GROWTH_ITEM || map[p->y][p->x] == POISON_ITEM)
+    {
+        eatItem(map[p->y][p->x], dir, map);
+    }
+    if (map[p->y][p->x] == WALL || map[p->y][p->x] == SNAKE_BODY)
+    {
+        map[p->y][p->x] = IMMUNE_WALL;
+        Gameover();
+    }
+    if (map[p->y][p->x] == GATE)
+    {
+        enterGate(p, map, dir);
+    }
+    map[p->y][p->x] = p->who;
+}
+
+void Stage::makeSnake(int **map)
+{
+    stat[0] = 3;
+    string itemIndex = " ^X@=+-%";
+    int row = 13;
+    int col = 26;
+    Bam = new Something(row, col--, SNAKE_BODY);
+    Something *p = new Something(row, col--, SNAKE_BODY);
+    Bam->link = p;
+    p = new Something(row, col--, SNAKE_HEAD);
+    Bam->link->link = p;
     map[Bam->y][Bam->x] = Bam->who;
-    Body++;
-  }
-  else if(item == 6){
-    map[Bam->y][Bam->x] = 0;
-    Bam = Bam->link;
-    Body--;
-  }
+    p = Bam->link;
+    map[p->y][p->x] = p->who;
+    p = p->link;
+    map[p->y][p->x] = p->who;
 }
 
-void Stage::Gameover(){
-  finish = true;
+void Stage::eatItem(int item, int dir, int **map)
+{
+    if (item == GROWTH_ITEM)
+    {
+        if (stat[0] == 10)
+        {
+            return;
+        }
+        Something *p = new Something(Bam->y, Bam->x, SNAKE_BODY);
+        if (dir == LEFT)
+            p->x++;
+        else if (dir == UP)
+            p->y++;
+        else if (dir == RIGHT)
+            p->x--;
+        else if (dir == DOWN)
+            p->y--;
+        p->link = Bam;
+        Bam = p;
+        map[Bam->y][Bam->x] = Bam->who;
+        stat[0]++;
+        stat[1]++;
+    }
+    else if (item == POISON_ITEM)
+    {
+        map[Bam->y][Bam->x] = EMPTY;
+        Bam = Bam->link;
+        stat[0]--;
+        stat[2]++;
+    }
 }
 
-void Stage::Gate(Something* head,int** map,int dir){
-  int root;
-  if(gate1->x == head->x && gate1->y == head->y){
-    if(gate2->x == 0){
-      head->x = 1; head->y = gate2->y;
-    }
-    else if(gate2->x == 49){
-      head->x = 48; head->y = gate2->y;
-    }
-    else if(gate2->y == 0){
-      head->x = gate2->x; head->y = 1;
-    }
-    else if(gate2->y == 24){
-      head->x = gate2->x; head->y = 23;
-    }
-    else{// 중간벽에 게이트가 있을시
-      root = findRoot(gate2,dir,map);
-      if(root == 65){
-        head->x = gate2->x-1;
-        head->y = gate2->y;
-      }
-      else if(root == 87){
-        head->x = gate2->x;
-        head->y = gate2->y-1;
-      }
-      else if(root == 68){
-        head->x = gate2->x+1;
-        head->y = gate2->y;
-      }
-      else if(root == 83){
-        head->x = gate2->x;
-        head->y = gate2->y+1;
-      }
-    }
-  }
-  else if(gate2->x == head->x && gate2->y == head->y){
-    if(gate1->x == 0){
-      head->x = 1; head->y = gate1->y;
-    }
-    else if(gate1->x == 49){
-      head->x = 48; head->y = gate1->y;
-    }
-    else if(gate1->y == 0){
-      head->x = gate1->x; head->y = 1;
-    }
-    else if(gate1->y == 24){
-      head->x = gate1->x; head->y = 23;
-    }
-    else{ // 중간벽에 게이트가 있을시
-      root = findRoot(gate1,dir,map);
-      if(root == 65){
-        head->x = gate1->x-1;
-        head->y = gate1->y;
-      }
-      else if(root == 87){
-        head->x = gate1->x;
-        head->y = gate1->y-1;
-      }
-      else if(root == 68){
-        head->x = gate1->x+1;
-        head->y = gate1->y;
-      }
-      else if(root == 83){
-        head->x = gate1->x;
-        head->y = gate1->y+1;
-      }
-    }
-  }
+void Stage::Gameover()
+{
+    finish = true;
 }
 
-int Stage::findRoot(Something* gate,int dir,int** map){
-  for(int i =0;i<4;i++){
-    if(dir == 65){
-      if(map[gate->y][gate->x -1] == 0) return dir;
-      else dir = 87;
+void Stage::alert(int color, int bkgdColor, const string msg)
+{
+    WINDOW *alert = newwin(7, 24, 13, 20);
+    box(alert, 0, 0);
+    wattron(alert, COLOR_PAIR(color));
+    wbkgd(alert, COLOR_PAIR(bkgdColor));
+    mvwprintw(alert, 3, 7, msg.c_str());
+    wrefresh(alert);
+    usleep(1750000);
+}
+
+void Stage::enterGate(Something *head, int **map, int dir)
+{
+    int root;
+    if (gate1->x == head->x && gate1->y == head->y)
+    {
+        if (gate2->x == 0)
+        {
+            head->x = 1;
+            head->y = gate2->y;
+        }
+        else if (gate2->x == COL_END)
+        {
+            head->x = COL_END - 1;
+            head->y = gate2->y;
+        }
+        else if (gate2->y == 0)
+        {
+            head->x = gate2->x;
+            head->y = 1;
+        }
+        else if (gate2->y == ROW_END)
+        {
+            head->x = gate2->x;
+            head->y = ROW_END - 1;
+        }
+        else
+        { // 중간벽에 게이트가 있을시
+            root = findRoot(gate2, dir, map);
+            if (root == LEFT)
+            {
+                head->x = gate2->x - 1;
+                head->y = gate2->y;
+            }
+            else if (root == UP)
+            {
+                head->x = gate2->x;
+                head->y = gate2->y - 1;
+            }
+            else if (root == RIGHT)
+            {
+                head->x = gate2->x + 1;
+                head->y = gate2->y;
+            }
+            else if (root == DOWN)
+            {
+                head->x = gate2->x;
+                head->y = gate2->y + 1;
+            }
+        }
     }
-    else if(dir == 87){
-      if(map[gate->y -1][gate->x] == 0) return dir;
-      else dir = 68;
+    else if (gate2->x == head->x && gate2->y == head->y)
+    {
+        if (gate1->x == 0)
+        {
+            head->x = 1;
+            head->y = gate1->y;
+        }
+        else if (gate1->x == COL_END)
+        {
+            head->x = COL_END - 1;
+            head->y = gate1->y;
+        }
+        else if (gate1->y == 0)
+        {
+            head->x = gate1->x;
+            head->y = 1;
+        }
+        else if (gate1->y == ROW_END)
+        {
+            head->x = gate1->x;
+            head->y = ROW_END - 1;
+        }
+        else
+        { // 중간벽에 게이트가 있을시
+            root = findRoot(gate1, dir, map);
+            if (root == LEFT)
+            {
+                head->x = gate1->x - 1;
+                head->y = gate1->y;
+            }
+            else if (root == UP)
+            {
+                head->x = gate1->x;
+                head->y = gate1->y - 1;
+            }
+            else if (root == RIGHT)
+            {
+                head->x = gate1->x + 1;
+                head->y = gate1->y;
+            }
+            else if (root == DOWN)
+            {
+                head->x = gate1->x;
+                head->y = gate1->y + 1;
+            }
+        }
     }
-    else if(dir == 68){
-      if(map[gate->y][gate->x + 1]== 0) return dir;
-      else dir = 83;
+    stat[3]++;
+}
+
+int Stage::findRoot(Something *gate, int dir, int **map)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        if (dir == LEFT)
+        {
+            if (map[gate->y][gate->x - 1] == EMPTY)
+                return dir;
+            else
+                dir = KEY_UP;
+        }
+        else if (dir == KEY_UP)
+        {
+            if (map[gate->y - 1][gate->x] == EMPTY)
+                return dir;
+            else
+                dir = RIGHT;
+        }
+        else if (dir == RIGHT)
+        {
+            if (map[gate->y][gate->x + 1] == EMPTY)
+                return dir;
+            else
+                dir = DOWN;
+        }
+        else if (dir == DOWN)
+        {
+            if (map[gate->y + 1][gate->x] == EMPTY)
+                return dir;
+            else
+                dir = LEFT;
+        }
     }
-    else if(dir == 83){
-      if(map[gate->y +1][gate->x] == 0) return dir;
-      else dir = 65;
-    }
-  }
+    return dir;
 }
