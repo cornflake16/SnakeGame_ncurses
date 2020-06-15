fbab2eda413810507036ee3366c049c89c102531
Nakwon Yun <skrdnjs1102@gmail.com>
2020-06-16 02:09:05 +0900

    Update main.cpp
    틱(0.1)초 구현
    (세로 가로 비율 감안해서 틱 비율도 맞추려했으나, 뱀이 위 아래 이동시 속도체감이 확 느껴져서 0.1초로 통일)

--- main.cpp
+++ main.cpp
@@ -1,39 +1,62 @@
 #include "Stage.h"
 int **map;
 int key, t = 0;
+int dir = LEFT;
 
 int main()
 {
     Stage view = Stage();
     view.setMap(view.stage);
-    for(int i=0; i<view.getStageNum(); i++)
+    for (int i = 0; i < view.getStageNum(); i++)
     {
-        mvprintw(3, 23, "[ Stage %d / %d ]", i+1, view.getStageNum());
+        mvprintw(3, 23, "[ Stage %d / %d ]", i + 1, view.getStageNum());
         map = view.copyMap(i);
         view.setMission();
         view.appearGate(map);
         view.makeSnake(map);
         pair<int, int> nItems = view.numOfItems(map);
-        while(1)
+        while (1)
         {
             if (!(++t % 10) && (nItems.first + nItems.second) < 4)
                 view.appearItem(map);
             if (view.stat[0] < 3)
                 view.Gameover();
-            view.move(key, map);
             view.drawMap(map);
-            if(view.isMissionClear())
+            switch (key = getch())
+            {
+            case LEFT:
+                if (dir != RIGHT)
+                    dir = LEFT;
+                break;
+            case UP:
+                if (dir != DOWN)
+                    dir = UP;
+                break;
+            case RIGHT:
+                if (dir != LEFT)
+                    dir = RIGHT;
+                break;
+            case DOWN:
+                if (dir != UP)
+                    dir = DOWN;
+                break;
+            case ESC:
+                endwin();
+                return 0;
+            }
+            view.move(dir, map);
+            if (view.isMissionClear())
             {
                 view.alert(0, 2, "Stage Clear!");
                 break;
             }
-            if((key=getch()) == 27 || view.checkfinish())
+            if (view.checkfinish())
                 goto end;
-            timeout(500);
+            timeout(100);
         };
     }
-    
-    end:
+
+end:
     view.alert(0, 2, "Game Over!");
     endwin();
     return 0;
