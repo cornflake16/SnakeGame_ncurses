c02c10231e1e450386b9b45aba763d0c68b622af
Nakwon Yun <skrdnjs1102@gmail.com>
2020-06-16 10:25:02 +0900

    Update main.cpp
    스테이지 넘어갔을 때, 게임오버되는 현상 해결.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "Stage.h"
 int **map;
-int key, t = 0;
-int dir = LEFT;
+int key, dir, t;
 
 int main()
 {
@@ -9,6 +8,7 @@ int main()
     view.setMap(view.stage);
     for (int i = 0; i < view.getStageNum(); i++)
     {
+        t = 0, dir = LEFT;
         mvprintw(3, 23, "[ Stage %d / %d ]", i + 1, view.getStageNum());
         map = view.copyMap(i);
         view.setMission();
@@ -19,8 +19,6 @@ int main()
         {
             if (!(++t % 10) && (nItems.first + nItems.second) < 4)
                 view.appearItem(map);
-            if (view.stat[0] < 3)
-                view.Gameover();
             view.drawMap(map);
             switch (key = getch())
             {
@@ -45,19 +43,24 @@ int main()
                 return 0;
             }
             view.move(dir, map);
+            if (view.stat[0] < 3)
+                view.Gameover();
             if (view.isMissionClear())
             {
                 view.alert(0, 2, "Stage Clear!");
                 break;
             }
-            if (view.checkfinish())
-                goto end;
+            if (view.checkGameOver())
+            {
+                view.alert(0, 2, "Game Over!");
+                endwin();
+                return 0;
+            }
             timeout(100);
         };
     }
 
 end:
-    view.alert(0, 2, "Game Over!");
     endwin();
     return 0;
 }
