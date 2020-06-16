b859acad8cdae37cb30a60063c0852892583af75
Nakwon Yun <skrdnjs1102@gmail.com>
2020-06-16 11:47:10 +0900

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,6 @@
 #include "Stage.h"
 int **map;
-int key, t = 0;
+int key, t;
 
 int main()
 {
@@ -8,56 +8,59 @@ int main()
     view.setMap(view.stage);
     for (int i = 0; i < view.getStageNum(); i++)
     {
+        t = 0;
         mvprintw(3, 23, "[ Stage %d / %d ]", i + 1, view.getStageNum());
         map = view.copyMap(i);
         view.setMission();
         view.appearGate(map);
         view.makeSnake(map);
         pair<int, int> nItems = view.numOfItems(map);
-        view.Setdir(LEFT);
         while (1)
         {
             if (!(++t % 10) && (nItems.first + nItems.second) < 4)
                 view.appearItem(map);
-            if (view.stat[0] < 3)
-                view.Gameover();
             view.drawMap(map);
             switch (key = getch())
             {
             case LEFT:
-                if (view.Getdir() != RIGHT)
-                    view.Setdir(LEFT);
+                if (view.dir != RIGHT)
+                    view.dir = LEFT;
                 break;
             case UP:
-                if (view.Getdir() != DOWN)
-                    view.Setdir(UP);
+                if (view.dir != DOWN)
+                    view.dir = UP;
                 break;
             case RIGHT:
-                if (view.Getdir() != LEFT)
-                    view.Setdir(RIGHT);
+                if (view.dir != LEFT)
+                    view.dir = RIGHT;
                 break;
             case DOWN:
-                if (view.Getdir() != UP)
-                    view.Setdir(DOWN);
+                if (view.dir != UP)
+                    view.dir = DOWN;
                 break;
-            case 29:
+            case ESC:
                 endwin();
                 return 0;
             }
             view.move(map);
+            if (view.stat[0] < 3)
+                view.Gameover();
             if (view.isMissionClear())
             {
                 view.alert(0, 2, "Stage Clear!");
                 break;
             }
-            if (view.checkfinish())
-                goto end;
+            if (view.checkGameOver())
+            {
+                view.alert(0, 2, "Game Over!");
+                endwin();
+                return 0;
+            }
             timeout(100);
         };
     }
 
 end:
-    view.alert(0, 2, "Game Over!");
     endwin();
     return 0;
 }
