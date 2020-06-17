3b00f463e8818dd073076c0257f64c57374c5850
Nakwon Yun <skrdnjs1102@gmail.com>
2020-06-17 23:43:00 +0900

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,6 @@
 #include "Stage.h"
 int key, t, n;
+
 int main()
 {
     Stage view = Stage();
@@ -12,25 +13,9 @@ int main()
         view.setMission();
         view.makeSnake();
         view.appearGate();
+        view.drawMap();
         while (1)
         {
-            view.moveSnake();
-            if(view.chkEnter)   // 게이트에 진입 후, 꼬리 부분까지 진출 성공 시, 문 재생성
-            {   
-                if(++n >= view.stat[0])
-                {
-                    view.disappearGate();
-                    view.appearGate();
-                    n = 0;
-                    view.chkEnter = FALSE;
-                }
-            }
-            if(++t%50 == 0) // 5초마다 아이템 재생성 (쓰레드 구현이 아니므로, 미세한 오차가 존재)
-            {
-                view.disappearItem();
-                view.appearItem();
-            }
-            view.drawMap();
             switch (key = getch())
             {
             case LEFT:
@@ -53,8 +38,26 @@ int main()
                 endwin();
                 return 0;
             }
-            if (view.stat[0] < 3) 
+            view.moveSnake();
+            if (view.chkEnter) // 게이트에 진입 후, 꼬리 부분까지 진출 성공 시, 문 재생성
+            {
+                if (++n >= view.stat[0])
+                {
+                    view.disappearGate();
+                    view.appearGate();
+                    n = 0;
+                    view.chkEnter = FALSE;
+                }
+            }
+            if (++t % 50 == 0) // 5초마다 아이템 재생성 (쓰레드 구현이 아니므로, 미세한 오차가 존재)
+            {
+                view.disappearItem();
+                view.appearItem();
+            }
+            if (view.stat[0] < 3) // 뱀의 길이가 3미만일 경우, 게임 오버
+            {
                 view.Gameover();
+            }
             if (view.isMissionClear())
             {
                 view.alert(0, 2, "Stage Clear!");
@@ -66,8 +69,9 @@ int main()
                 endwin();
                 return 0;
             }
-            timeout(100);   // 0.1s 딜레이
-        };
+            view.drawMap();
+            timeout(100); // 0.1s 딜레이
+        }
     }
     endwin();
     return 0;
